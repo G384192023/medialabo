ca6bd6c9f384080934f7291104438c2cf8e8da76
G384192023 <238419@st.takushoku-u.ac.jp>
2025-05-15 16:06:48 +0900

    jnb

--- kazuate.js
+++ kazuate.js
@@ -3,21 +3,17 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
+let kaisu = span#kaisu;
 
 // 予想を4回実行する
 
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
     // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-    let yoso = 4;
+    <input type="text" name="shimei" size="10" id="yoso">
 
     let seikai = false;
   
