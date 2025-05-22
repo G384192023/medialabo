806fd85300c68b8a303100ad63627a0d74183329
G384192023 <238419@st.takushoku-u.ac.jp>
2025-05-22 14:35:01 +0900

    lo

--- kazuatea.js
+++ kazuatea.js
@@ -4,48 +4,48 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
- 
+
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-let c = document.querySelector('button#hantei');
-c.addEventListener('click',hantei);
- 
+let b = document.querySelector('button#hantei');
+b.addEventListener('click',hantei);
+
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei(){
+function hantei() {
 
     // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
     let yoso = document.querySelector('input[name="shimei"]');
-    let y = Number(yoso.value);
+    let n = Number(yoso.value);
+    let r = document.querySelector('span#answer');
+    r.textContent = n;
     let A;
-    let a1 = document.querySelector('span#kaisu');
-    a1.textContent = kaisu;
-    let a2 = document.querySelector('span#answer');
-    a2.textContent = yoso;
-    let b = document.querySelector('p#result');
-    b.textContent = A;
-    
+
     // 課題3-1: 正解判定する
-    kaisu += 1;
-    console.log(kaisu + '回目の予想:' + yoso);
-    A = (kaisu + '回目の予想:' + yoso);
- 
-    if (kaisu >= 4 ){
+    kaisu++;
+    let k = document.querySelector('span#kaisu');
+    k.textContent=kaisu;
+    let a = document.querySelector('p#result');
+
+    console.log(kaisu + "回目の予想: " + yoso);
+    A = (kaisu + "回目の予想:" + yoso);
+
+    if (kaisu >= 4){
         console.log("答えは "+kotae+" でした.すでにゲームは終わっています");
         A = ("答えは "+kotae+" でした.すでにゲームは終わっています");
-    }else if (kotae == Number(yoso)){
+    }else if (kotae == Number(yoso)) {
         console.log("正解です. おめでとう！");
-        A = ("正解です. おめでとう！");
-    }else if (kotae == 3){
+        A=("正解です. おめでとう！");
+    }else if (kotae == 3) {
         console.log("まちがい.残念でした. 答えは "+kotae+" です.");
         A = ("まちがい.残念でした. 答えは "+kotae+" です.");
     }else {
-        if (yoso < kotae){
+        if (yoso < kotae) {
             console.log("まちがい. 答えはもっと大きいですよ");
             A = ("まちがい. 答えはもっと大きいですよ");
-        }else{
+        }else {
             console.log("まちがい. 答えはもっと小さいですよ");
-            A = ("まちがい. 答えはもっと小さいですよ");
+            A =( "まちがい. 答えはもっと小さいですよ");
         }
-    }
+    }   
 }
