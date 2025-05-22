7a7bb00260d5ed2aaca3bac2c147ae87471ef9c2
G384192023 <238419@st.takushoku-u.ac.jp>
2025-05-22 14:57:37 +0900

    dcc

--- kazuateE.js
+++ kazuateE.js
@@ -8,46 +8,48 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-let z1=document.querySelector('button');
-z1.addEventListener('click',hantei);
-
+let c = document.querySelector('button#hantei');
+c.addEventListener('click',hantei);
+ 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
 
     // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-    let i=document.querySelector('input[name="shimei"]');
-    let yoso=i.value;
+    let i = document.querySelector('input[name="shimei"]');
+    let yoso = i.value;
     let A;
-
+    
+    
     // 課題3-1: 正解判定する
-    kaisu += 1;
-    console.log(kaisu+'回目の予想:'+yoso);
-    A=(kaisu+'回目の予想:'+yoso);
+    
+
+    console.log(kaisu + '回目の予想:' + yoso);
+    A = (kaisu + '回目の予想:' + yoso);
  
-    if(kaisu>=4){
+    if (kaisu >= 4 ){
         console.log("答えは "+kotae+" でした.すでにゲームは終わっています");
-        A=("答えは "+kotae+" でした.すでにゲームは終わっています");
-    }else if(kotae==Number(yoso)){
+        A = ("答えは "+kotae+" でした.すでにゲームは終わっています");
+    }else if (kotae == Number(yoso)){
         console.log("正解です. おめでとう！");
-        A=("正解です. おめでとう！");
-    }else if(kotae==3){
+        A = ("正解です. おめでとう！");
+    }else if (kotae == 3){
         console.log("まちがい.残念でした. 答えは "+kotae+" です.");
-        A=("まちがい.残念でした. 答えは "+kotae+" です.");
-    }else{
-        if(yoso<kotae){
+        A = ("まちがい.残念でした. 答えは "+kotae+" です.");
+    }else {
+        if (yoso < kotae){
             console.log("まちがい. 答えはもっと大きいですよ");
-            A=("まちがい. 答えはもっと大きいですよ");
+            A = ("まちがい. 答えはもっと大きいですよ");
         }else{
             console.log("まちがい. 答えはもっと小さいですよ");
-            A=("まちがい. 答えはもっと小さいですよ");
+            A = ("まちがい. 答えはもっと小さいですよ");
         }
     }
- 
-    let x1=document.querySelector('span#kaisu');
-    x1.textContent=kaisu;
-    let x2=document.querySelector('span#answer');
-    x2.textContent=yoso;
- 
-    let y1=document.querySelector('p#result');
-    y1.textContent=A;
+    let a2 = document.querySelector('span#answer');
+    a2.textContent = yoso;
+    
+    kaisu += 1;
+    let a1 = document.querySelector('span#kaisu');
+    a1.textContent = kaisu;
+    let b = document.querySelector('p#result');
+    b.textContent = A;
 }
