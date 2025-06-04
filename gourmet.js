2b979f7728452a8556d5ba4841971bbab3de6cb0
G384192023 <238419@st.takushoku-u.ac.jp>
2025-06-04 10:52:29 +0900

    nk

--- gourmet.js
+++ gourmet.js
@@ -105,57 +105,55 @@ function showResult(resp) {
 
   let div = document.querySelector('div#result');
 
-  let resultCount = data.results.shop.length;
-  let resultCountMessage = document.createElement('h3');
-  resultCountMessage.textContent = `検索結果: ${resultCount}件ヒットしました。`;
-  div.appendChild(resultCountMessage);
+  let resultcount = data.results.shop.length;
+  let resultcountmessage = document.createElement('h3');
+  resultcountmessage.textContent = `検索結果: ${resultcount}件ヒットしました。`;
+  div.appendChild(resultcountmessage);
 
   data.results.shop.forEach((shop, index) => {
     let h = document.createElement('h3');
-    h.textContent = `検索結果${index + 1}件目`;
+    h.textContent = `検索結果 ${index + 1}件目`;
     div.appendChild(h);
 
     let ul = document.createElement('ul');
 
     let li1 = document.createElement('li');
     li1.setAttribute('id', 'name');
+    li1.textContent = "名前: " + shop.name;
+    ul.appendChild(li1);
     let li2 = document.createElement('li');
     li2.setAttribute('id', 'access');
+    li2.textContent = "アクセス: " + shop.access;
+    ul.appendChild(li2);
     let li3 = document.createElement('li');
     li3.setAttribute('id', 'address');
+    li3.textContent = "住所: " + shop.address;
+    ul.appendChild(li3);
     let li4 = document.createElement('li');
     li4.setAttribute('id', 'budget');
+    li4.textContent = "予算: " + shop.budget.name;
+    ul.appendChild(li4);
     let li5 = document.createElement('li');
     li5.setAttribute('id', 'catch');
+    li5.textContent = "キャッチコピー: " + shop.catch;
+    ul.appendChild(li5);
     let li6 = document.createElement('li');
     li6.setAttribute('id', 'genre');
+    li6.textContent = "ジャンル: " + shop.genre.name;
+    ul.appendChild(li6);
     let li7 = document.createElement('li');
     li7.setAttribute('id', 'open');
+    li7.textContent = "営業時間: " + shop.open;
+    ul.appendChild(li7);
     let li8 = document.createElement('li');
     li8.setAttribute('id', 'station');
+    li8.textContent = "最寄駅: " + shop.station_name;
+    ul.appendChild(li8);
     let li9 = document.createElement('li');
     li9.setAttribute('id', 'sub-genre');
-
-    li1.textContent = "名前: " + shop.name;
-    li2.textContent = "アクセス: " + shop.access;
-    li3.textContent = "住所: " + shop.address;
-    li4.textContent = "予算: " + shop.budget.name;
-    li5.textContent = "キャッチコピー: " + shop.catch;
-    li6.textContent = "ジャンル: " + shop.genre.name;
-    li7.textContent = "営業時間: " + shop.open;
-    li8.textContent = "最寄駅: " + shop.station_name;
     li9.textContent = "サブジャンル: " + (shop.sub_genre ? shop.sub_genre.name : 'なし');
-
-    ul.appendChild(li1);
-    ul.appendChild(li2);
-    ul.appendChild(li3);
-    ul.appendChild(li4);
-    ul.appendChild(li5);
-    ul.appendChild(li6);
-    ul.appendChild(li7);
-    ul.appendChild(li8);
     ul.appendChild(li9);
-
+    
     div.appendChild(ul);
   });
 }
@@ -167,7 +165,7 @@ function showError(err) {
 
 // 課題6-1: 通信の最後にいつも実行する処理
 function finish() {
-    console.log('Ajax 通信が終わりました');
+    console.log('通信が終わりました');
 }
 
 ////////////////////////////////////////
